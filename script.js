b09ec07f8abd920c51718e5ff082c84d56505fba
Richard Nguyen <nguyenrichard972@gmail.com>
2022-07-20 14:31:49 -0700

    Add hover effect on div

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('#container');
 const gridCol = 'auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto auto';
-container.setAttribute('style', `display: grid; grid-template-columns: ${gridCol}; width: 650px; height: 650px; border: 1px solid gray`);
+container.setAttribute('style', `display: grid; grid-template-columns: ${gridCol}; width: 500px; height: 500px; border: 1px solid gray`);
 
 for (let i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.setAttribute('style', 'border: 1px solid gray;');
+    square.addEventListener('mouseenter', () => {
+        square.setAttribute('style', 'background-color: black');
+    });
     container.append(square);
 }
